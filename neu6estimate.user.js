c6aeb4c5f64d766de8cb0367f81d989d807336a8
popcorner <6312791+popcorner@users.noreply.github.com>
2016-10-22 19:32:18 +0800

    Create neu6estimate.user.js

---
+++ neu6estimate.user.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name		6V热度估测器
+// @name:en		neu6estimate
+// @namespace	http://github.com/popcorner/neu6estimate
+// @description	estimate
+// @version		0.1.0
+// @author		popcorner
+// @grant		none
+// @include		http://bt.neu6.edu.cn/thread*
+// @include		http://bt.neu6.edu.cn/forum.php?mod=viewthread*
+// @icon		http://bt.neu6.edu.cn/favicon.ico
+// @updateURL	none
+// @downloadURL	none
+// ==/UserScript==
